f54e7b5ecfef6fc9d2351abcf9d58beefa6df878
slavazeboldd-spec <slavazeboldd@gmail.com>
2025-10-30 03:54:13 +0500

    Update task1.py

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    word = input()
+result = (word + ' ') * 4
+print(result.strip())
         
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
