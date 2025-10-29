3ed79306536f82acea7e6ad953ee3e7a653b41dd
slavazeboldd-spec <slavazeboldd@gmail.com>
2025-10-30 04:03:26 +0500

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+first_line = input()
+second_line = input()
+result = second_line + first_line
+print(result)     
 
 
 # Код ниже не трогать! он нужен для тестов
